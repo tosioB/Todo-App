79b2b702bc0f56f820fea592ed8e015ffb420015
kimdaesik <ds500033@naver.com>
2024-07-29 16:59:10 +0900

    투두리스트 만들기

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,11 @@ function App() {
 
   return (
     <>
-      <TodoList todoList={todoList} setTodoList={setTodoList} />
-      <hr />
-      <TodoInput todoList={todoList} setTodoList={setTodoList} />
+      <div className="todo-box">
+        <h1 className="title">TodoList</h1>
+        <TodoList todoList={todoList} setTodoList={setTodoList} />
+        <TodoInput todoList={todoList} setTodoList={setTodoList} />
+      </div>
     </>
   );
 }
@@ -22,27 +24,32 @@ function TodoInput({ todoList, setTodoList }) {
 
   return (
     <>
-      <input
-        value={inputValue}
-        onChange={(event) => setInputValue(event.target.value)}
-      />
-      <button
-        onClick={() => {
-          const newTodo = { id: Number(new Date()), content: inputValue };
-          const newTodoList = [...todoList, newTodo];
-          setTodoList(newTodoList);
-          setInputValue("");
-        }}
-      >
-        추가하기
-      </button>
+      <div className="add-box">
+        <input
+          className="add-inp"
+          type="text"
+          value={inputValue}
+          onChange={(event) => setInputValue(event.target.value)}
+        />
+        <button
+          className="btn add-btn"
+          onClick={() => {
+            const newTodo = { id: Number(new Date()), content: inputValue };
+            const newTodoList = [...todoList, newTodo];
+            setTodoList(newTodoList);
+            setInputValue("");
+          }}
+        >
+          추가하기
+        </button>
+      </div>
     </>
   );
 }
 
 function TodoList({ todoList, setTodoList }) {
   return (
-    <ul>
+    <ul className="todo-list">
       {todoList.map((todo) => (
         <Todo key={todo.id} todo={todo} setTodoList={setTodoList} />
       ))}
@@ -52,33 +59,68 @@ function TodoList({ todoList, setTodoList }) {
 
 function Todo({ todo, setTodoList }) {
   const [inputValue, setInputValue] = useState("");
+  const [inputStatus, setInputStatus] = useState(false);
   return (
     <li>
-      {todo.content}
-      <input
-        value={inputValue}
-        onChange={(event) => setInputValue(event.target.value)}
-      />
-      <button
-        onClick={() => {
-          setTodoList((prev) =>
-            prev.map((el) =>
-              el.id === todo.id ? { ...el, content: inputValue } : el
-            )
-          );
-        }}
-      >
-        수정
-      </button>
-      <button
-        onClick={() => {
-          setTodoList((prev) => {
-            return prev.filter((el) => el.id !== todo.id);
-          });
-        }}
-      >
-        삭제
-      </button>
+      <div className="text-box">
+        <input type="checkbox" name="checkbox" id={`check${todo.id}`} className="complete-check" />
+        <label htmlFor={`check${todo.id}`} className="todo-content">{todo.content}</label>
+        
+        {
+          inputStatus && 
+          <input
+            type="text"
+            className="edit-inp"
+            value={inputValue}
+            onChange={(event) => setInputValue(event.target.value)}
+          />
+        }
+      </div>
+      
+      <div className="btn-box">
+        {
+          !inputStatus && 
+          <button
+            className="btn edit-btn"
+            onClick={() => {
+              setInputStatus(true)
+            }}
+          >
+            수정
+          </button>
+        }
+        {
+          inputStatus && 
+          <button
+            className="btn complete-btn"
+            onClick={() => {
+              if (inputValue !== '') {
+                setInputStatus(false)
+                setTodoList((prev) =>
+                  prev.map((el) =>
+                    el.id === todo.id ? { ...el, content: inputValue } : el
+                  )
+                );
+                setInputValue('');
+              } else {
+                alert('수정 내용을 입력하세요.');
+              }
+            }}
+          >
+            완료
+          </button>
+        }
+        <button
+          className="btn del-btn"
+          onClick={() => {
+            setTodoList((prev) => {
+              return prev.filter((el) => el.id !== todo.id);
+            });
+          }}
+        >
+          삭제
+        </button>
+      </div>
     </li>
   );
 }
